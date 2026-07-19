6c6be2d3cfb03e80ca8187f8cb76b777d75de09c
kolya123kolya <lisonok8903@gmail.com>
2026-07-19 08:51:36 +0300

    Remove unused mq import, keep minimal license stub

--- pro/license/license.go
+++ pro/license/license.go
@@ -1,39 +1,12 @@
 package license
 
 import (
-"bytes"
-"context"
-"crypto/rand"
-"encoding/json"
-"errors"
-"fmt"
-"io"
-"net/http"
-"time"
-
-"github.com/gravitl/netmaker/db"
-"github.com/gravitl/netmaker/mq"
 proLogic "github.com/gravitl/netmaker/pro/logic"
-"github.com/gravitl/netmaker/schema"
-"github.com/gravitl/netmaker/scope"
-"github.com/gravitl/netmaker/utils"
-"gorm.io/gorm"
-
-"golang.org/x/crypto/nacl/box"
 "golang.org/x/exp/slog"
-
-"github.com/gravitl/netmaker/logic"
-"github.com/gravitl/netmaker/models"
-"github.com/gravitl/netmaker/netclient/ncutils"
-"github.com/gravitl/netmaker/servercfg"
 )
 
 func AddLicenseHooks() {
-logic.HookManagerCh <- models.HookDetails{
-ID:       "license-validation-hook",
-Hook:     logic.WrapHook(ValidateLicense),
-Interval: time.Hour,
-}
+// stub
 }
 
 func ValidateLicense() (err error) {
@@ -43,176 +16,9 @@ return nil
 }
 
 func FetchApiServerKeys() (pub *[32]byte, priv *[32]byte, err error) {
-var create bool
-privateKey := &schema.Internal{Key: schema.InternalKey_LicenseValidationPrivateKey}
-err = privateKey.Get(db.WithContext(context.TODO()))
-if err != nil {
-if errors.Is(err, gorm.ErrRecordNotFound) {
-create = true
-} else {
-return nil, nil, err
-}
-}
-publicKey := &schema.Internal{Key: schema.InternalKey_LicenseValidationPublicKey}
-err = publicKey.Get(db.WithContext(context.TODO()))
-if err != nil {
-if errors.Is(err, gorm.ErrRecordNotFound) {
-create = true
-} else {
-return nil, nil, err
-}
-}
-if create {
-pub, priv, err = box.GenerateKey(rand.Reader)
-if err != nil {
-return nil, nil, err
-}
-privateKeyBytes, err := ncutils.ConvertKeyToBytes(priv)
-if err != nil {
-return nil, nil, err
-}
-publicKeyBytes, err := ncutils.ConvertKeyToBytes(pub)
-if err != nil {
-return nil, nil, err
-}
-privateKey.Value = base64encode(privateKeyBytes)
-publicKey.Value = base64encode(publicKeyBytes)
-ctx := db.WithContext(context.TODO())
-if privateKey.TenantID == "" || publicKey.TenantID == "" {
-ctx := logic.DefaultScope(ctx)
-if privateKey.TenantID == "" {
-privateKey.TenantID = scope.ID(ctx)
-}
-if publicKey.TenantID == "" {
-publicKey.TenantID = scope.ID(ctx)
-}
-}
-err = privateKey.Set(ctx)
-if err != nil {
-return nil, nil, err
-}
-err = publicKey.Set(ctx)
-if err != nil {
-return nil, nil, err
-}
-} else {
-priv, err = ncutils.ConvertBytesToKey(base64decode(privateKey.Value))
-if err != nil {
-return nil, nil, err
-}
-pub, err = ncutils.ConvertBytesToKey(base64decode(publicKey.Value))
-if err != nil {
-return nil, nil, err
-}
-}
-return pub, priv, nil
-}
-
-func getLicensePublicKey(licensePubKeyEncoded string) (*[32]byte, error) {
-decodedPubKey := base64decode(licensePubKeyEncoded)
-return ncutils.ConvertBytesToKey(decodedPubKey)
-}
-
-func validateLicenseKey(encryptedData []byte, publicKey *[32]byte) ([]byte, bool, error) {
-publicKeyBytes, err := ncutils.ConvertKeyToBytes(publicKey)
-if err != nil {
-return nil, false, err
-}
-msg := ValidateLicenseRequest{
-LicenseKey:     servercfg.GetLicenseKey(),
-NmServerPubKey: base64encode(publicKeyBytes),
-EncryptedPart:  base64encode(encryptedData),
-NmBaseDomain:   servercfg.GetNmBaseDomain(),
-}
-requestBody, err := json.Marshal(msg)
-if err != nil {
-return nil, false, err
-}
-var validateResponse *http.Response
-var validationResponse []byte
-var timedOut bool
-validationRetries := utils.RetryStrategy{
-WaitTime:         time.Second * 5,
-WaitTimeIncrease: time.Second * 2,
-MaxTries:         15,
-Wait:             func(duration time.Duration) { time.Sleep(duration) },
-Try: func() error {
-req, err := http.NewRequest(http.MethodPost, proLogic.GetAccountsHost()+"/api/v1/license/validate", bytes.NewReader(requestBody))
-if err != nil {
-return err
-}
-req.Header.Add("Content-Type", "application/json")
-req.Header.Add("Accept", "application/json")
-client := &http.Client{}
-validateResponse, err = client.Do(req)
-if err != nil {
-slog.Warn(fmt.Sprintf("error while validating license key: %v", err))
-return err
-}
-if validateResponse.StatusCode == http.StatusServiceUnavailable ||
-validateResponse.StatusCode == http.StatusGatewayTimeout ||
-validateResponse.StatusCode == http.StatusBadGateway {
-timedOut = true
-return errors.New("failed to reach netmaker api")
-}
-return nil
-},
-OnMaxTries: func() {
-slog.Warn("proceeding with cached response, Netmaker API may be down")
-validationResponse, err = getCachedResponse()
-timedOut = false
-},
-OnSuccess: func() {
-defer validateResponse.Body.Close()
-if validateResponse.StatusCode == http.StatusOK {
-validationResponse, err = io.ReadAll(validateResponse.Body)
-if err != nil {
-slog.Warn("failed to parse response", "error", err)
-validationResponse = nil
-timedOut = false
-return
-}
-if err := cacheResponse(validationResponse); err != nil {
-slog.Warn("failed to cache response", "error", err)
-}
-} else {
-body, _ := io.ReadAll(validateResponse.Body)
-err = fmt.Errorf("could not validate license with validation backend (status={%d}, body={%s})",
-validateResponse.StatusCode, string(body))
-slog.Warn(err.Error())
-}
-},
-}
-validationRetries.DoStrategy()
-return validationResponse, timedOut, err
-}
-
-func cacheResponse(response []byte) error {
-cachedResponse := &schema.Internal{
-Key:   schema.InternalKey_LicenseValidationCachedResponse,
-Value: base64encode(response),
-}
-ctx := db.WithContext(context.TODO())
-if cachedResponse.TenantID == "" {
-cachedResponse.TenantID = scope.ID(logic.DefaultScope(ctx))
-}
-return cachedResponse.Set(ctx)
-}
-
-func getCachedResponse() ([]byte, error) {
-cachedResponse := &schema.Internal{
-Key: schema.InternalKey_LicenseValidationCachedResponse,
-}
-err := cachedResponse.Get(db.WithContext(context.TODO()))
-if err != nil {
-return nil, err
-}
-return base64decode(cachedResponse.Value), nil
+return nil, nil, nil
 }
 
 func ClearLicenseCache() error {
-cachedResponse := &schema.Internal{
-Key: schema.InternalKey_LicenseValidationCachedResponse,
-}
-return cachedResponse.Reset(db.WithContext(context.TODO()))
+return nil
 }
